e8b3725386634cac99852b02c32bf6dc0a3fadd3
can-ishk <kchathley26@gmail.com>
2023-09-07 23:07:50 +0530

    init + setup

---
+++ contracts/ToDoList.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.5.0;
+
+contract ToDoList {
+    uint public taskCount = 0; // state variable to store the number of tasks
+    //scope of the variable belongs to the entire smart contract.
+}
